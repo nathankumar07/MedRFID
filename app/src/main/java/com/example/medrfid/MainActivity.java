5b473b8ee86419bd641b20ac2d77ced993294c20
sumuksr2 <43393177+sumuksr2@users.noreply.github.com>
2019-06-25 23:17:39 -0500

    Tempelate done with Physician Page

--- app/src/main/java/com/example/medrfid/MainActivity.java
+++ app/src/main/java/com/example/medrfid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.medrfid;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -7,14 +8,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         transparentStatusAndNavigation();
+
+        login = (Button) findViewById(R.id.btnLogIn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PhysicianHome.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     private void transparentStatusAndNavigation() {
